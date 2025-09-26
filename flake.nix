0c0dac8faefd368a101eea07eac23e3c7873d3aa
Vistac <vistac.wu@gmail.com>
2025-09-26 15:49:05 +0800

    aa

--- flake.nix
+++ flake.nix
@@ -6,7 +6,12 @@
   };
 
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    nixosConfigurations = {
+      nixos=lib.nixosSystem{
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+    };
   };
 }
